2871c766dc47d5260883b0f59d635cb60f2641bb
Arpita Das <arpita1477@gmail.com>
2026-06-22 21:55:48 +0600

    Add files via upload

---
+++ New_Pro_Coder.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    if (m * 2 >= n) cout << "NEWBIE" << endl;
+    else cout << "PRO" << endl;
+
+}
