cd7148d014ef0acd7ba5c5b52680bca49225f0ca
Arpita Das <arpita1477@gmail.com>
2026-07-27 19:06:17 +0600

    Add files via upload

---
+++ Advitiya.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int N;
+	cin>>N;
+	if(N==16 || N==17 || N==18) cout<<"ADVITIYA"<<endl;
+	else cout<<"WAITING FOR ADVITIYA"<<endl;
+
+}
