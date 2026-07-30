65c12f99d8a6cf183dab9695d1d736690de495c2
Arpita Das <arpita1477@gmail.com>
2026-07-30 20:37:18 +0600

    Add files via upload

---
+++ Varied_Digits.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    cin>>x;
+    int a=x%10;
+    int b=x/10;
+    if(a==b) cout<<"No"<<endl;
+    else cout<<"Yes"<<endl;
+
+}
