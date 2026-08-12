64bb646886c24ed270e9a37abc38531cc066315d
Arpita Das <arpita1477@gmail.com>
2026-08-12 22:06:08 +0600

    Add files via upload

---
+++ Check_Even.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int l,r;
+	cin>>l>>r;
+	if(l%2==0 || r%2==0 || l<r){
+	    cout<<"YES"<<endl;
+	}
+	else{
+	    cout<<"NO"<<endl;
+	}
+
+}
