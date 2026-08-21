e0691bd0742955aff3ce7f29fcee12432314b3bb
Arpita Das <arpita1477@gmail.com>
2026-08-21 22:04:58 +0600

    Add files via upload

---
+++ Mirror_Swap.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N;
+        cin >> N;
+
+        vector<int> A(2 * N);
+
+        for (int i = 0; i < 2 * N; i++) {
+            cin >> A[i];
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < N; i++) {
+            sum += max(A[i], A[2 * N - 1 - i]);
+        }
+
+        cout << sum << '\n';
+    }
+
+    return 0;
+}
