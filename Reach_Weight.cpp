edae1ab4670c2f1b160302932b42f93e06a14087
Arpita Das <arpita1477@gmail.com>
2026-09-03 19:21:48 +0600

    Add files via upload

---
+++ Reach_Weight.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N;
+        cin >> N;
+
+        int cost = (N / 2) * 30;
+
+        if (N % 2 == 1) {
+            cost += 20;
+        }
+
+        cout << cost << endl;
+    }
+
+    return 0;
+}
