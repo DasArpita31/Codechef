5ec0d7300d5e7a54eb2451683d30dc8b1a2bc0d8
Arpita Das <arpita1477@gmail.com>
2026-09-11 23:47:49 +0600

    Add files via upload

---
+++ Favourite_Numbers.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+
+    while (T--)
+    {
+        int A;
+        cin >> A;
+
+        if (A % 2 == 0 && A % 7 == 0)
+        {
+            cout << "Alice" << endl;
+        }
+        else if (A % 2 != 0 && A % 9 == 0)
+        {
+            cout << "Bob" << endl;
+        }
+        else
+        {
+            cout << "Charlie" << endl;
+        }
+    }
+
+    return 0;
+}
