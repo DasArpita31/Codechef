5ed1398172d6a3e0d45101d0237c6f19ddac1f16
Arpita Das <arpita1477@gmail.com>
2026-09-12 23:22:32 +0600

    Add files via upload

---
+++ Problem_Category.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int x;
+        cin >> x;
+
+        if (x < 100) {
+            cout << "Easy" << endl;
+        }
+        else if (x < 200) {
+            cout << "Medium" << endl;
+        }
+        else {
+            cout << "Hard" << endl;
+        }
+    }
+
+    return 0;
+}
