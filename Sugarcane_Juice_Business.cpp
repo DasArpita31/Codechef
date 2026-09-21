02f4daf6d56de28535829f5b6cd198b79893816b
Arpita Das <arpita1477@gmail.com>
2026-09-21 23:36:34 +0600

    Add files via upload

---
+++ Sugarcane_Juice_Business.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N;
+        cin >> N;
+
+        cout << N * 15 << endl;
+    }
+
+    return 0;
+}
