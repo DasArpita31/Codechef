f4971c9eea20f17cf1947d0ce5f0de65f47179e3
Arpita Das <arpita1477@gmail.com>
2026-09-23 23:27:40 +0600

    Add files via upload

---
+++ Chef_and_Candies.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N, X;
+        cin >> N >> X;
+
+        if (X >= N)
+            cout << 0 << endl;
+        else {
+            int needed = N - X;
+            cout << (needed + 3) / 4 << endl;
+        }
+    }
+
+    return 0;
+}
